933a0908333202064046f2eb5990cdc3d1f658d7
virat1969 <46090988+virat1969@users.noreply.github.com>
2019-05-26 20:17:30 -0400

    Update app.py
    Module 4 completed

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,33 @@
-from flask import Flask, render_template
+import sqllite3
+from flask import Flask, render_template, g
+
+PATH = 'db/jobs.sqllite'
 
 app = Flask(__name__)
+
+def open_connetion():
+    connection = getattr(g,'_connection', None)
+    if connection == None:
+        connection = g._connection = sqllite3.connect(PATH)
+    connection.row_factory = sqllite3.row_factory
+    return connection
+
+def execute_sql(sql,values=(), commit=False, single=False):
+    connection = open_connetion()
+    cursor = connection.execute(sql,values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 @app.route('/')
 @app.route('/jobs')
