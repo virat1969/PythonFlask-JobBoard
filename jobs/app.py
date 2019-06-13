41fc56b3a4563414e481a989c4f178e1faf63cfe
virat1969 <46090988+virat1969@users.noreply.github.com>
2019-06-13 10:24:14 -0400

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -58,7 +58,7 @@ def review(employer_id):
 
         date = datetime.datetime.now().strftime("%m/%d/%Y")
         execute_sql('INSERT into review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
-
+        
         return redirect(url_for('employer', employer_id=employer_id))
 
     return render_template('review.html', employer_id=employer_id)
